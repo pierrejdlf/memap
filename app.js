4a94d8faaa2da1e314df7b2bc548029202c64742
Pierre JdlF <pierre.jdlf@gmail.com>
2012-12-18 19:19:41 +0100

    ok

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ requirejs([
 	app.configure(function(){
 		app.use(express.favicon());
 		app.use(express.bodyParser());
-		app.use(express.static(__dirname + '/public'));
 		app.use(express.methodOverride());
 		app.use(app.router);
+		app.use(express.static(__dirname + '/public'));
 	});
 
 	app.configure('development', function(){
