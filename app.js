e732e7e57328e4efe672e4f04f1bdc48cedb9857
Pierre JdlF <pierre.jdlf@gmail.com>
2012-12-18 08:22:11 +0100

    ok

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ requirejs([
 
 	app.configure(function(){
 		app.use(express.favicon());
+		app.use(express.['static'](__dirname + '/public'));
 		app.use(express.bodyParser());
 		app.use(express.methodOverride());
 		app.use(app.router);
@@ -35,7 +36,7 @@ requirejs([
 
 	app.configure('development', function(){
 		app.use(express.logger('dev'));
-		app.use(express['static'](__dirname + '/public'));
+		app.use(express['static'](__dirname + '/public/'));
 		app.use(express.errorHandler({
 			dumpExceptions: true, 
 			showStack: true
